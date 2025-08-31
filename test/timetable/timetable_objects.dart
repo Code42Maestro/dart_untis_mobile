3ba80937df902d9d940949213d2ba7599b031b06
code42maestro <146424202+Code42Maestro@users.noreply.github.com>
2025-09-01 01:44:42 +0200

    Upgraded dependencies Added first test

---
+++ test/timetable/timetable_objects.dart
@@ -0,0 +1,10 @@
+import 'package:dart_untis_mobile/dart_untis_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('test UntisPeriodState parsing', () {
+    expect(UntisPeriodState.parse('regular'), UntisPeriodState.regular);
+    expect(UntisPeriodState.parse('irregular'), UntisPeriodState.irregular);
+    expect(UntisPeriodState.parse('cancelled'), UntisPeriodState.cancelled);
+  });
+}
